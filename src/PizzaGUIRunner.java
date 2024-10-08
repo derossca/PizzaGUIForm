8e519e57658325f833047c6ad609dfde0f56ca73
derossca <derossca@mail.uc.edu>
2024-10-08 14:10:21 -0400

    PizzaGUIRunner set up

--- src/PizzaGUIRunner.java
+++ src/PizzaGUIRunner.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 public class PizzaGUIRunner {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PizzaGUIFrame frame = new PizzaGUIFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
