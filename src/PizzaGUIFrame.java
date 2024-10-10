fc0fdea18f6187c9f2339d3890744350434d0514
derossca <derossca@mail.uc.edu>
2024-10-10 10:03:30 -0400

    made crust panel

--- src/PizzaGUIFrame.java
+++ src/PizzaGUIFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 
@@ -28,20 +29,48 @@ public class PizzaGUIFrame extends JFrame {
     JScrollPane scroll;
 
     JButton quitBtn;
+    JButton clearBtn;
     JButton dispalyOrderBtn;
+
+    JOptionPane quitPane;
+
+    ButtonGroup buttonGroup;
 
     public PizzaGUIFrame() {
         createCenterFrame();
-        setTitle("Pizza Order");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
 
         mainPnl = new JPanel();
+        mainPnl.setLayout(new BorderLayout());
 
+        createCrustPanel();
+        mainPnl.add(crustPnl, BorderLayout.EAST);
+
+        add(mainPnl, BorderLayout.CENTER);
+        setTitle("Pizza Order");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
 
     private void createCrustPanel() {
+        crustPnl = new JPanel();
+        crustPnl.setBorder(new TitledBorder(new EtchedBorder(), "Crust"));
 
+        thinCrustRB = new JRadioButton("Thin Crust");
+        regCrustRB = new JRadioButton("Regular Crust");
+        deepDishCrustRB = new JRadioButton("Deep Dish Crust");
+
+        buttonGroup = new ButtonGroup();
+        buttonGroup.add(thinCrustRB);
+        buttonGroup.add(regCrustRB);
+        buttonGroup.add(deepDishCrustRB);
+
+        thinCrustRB.setSelected(false);
+        regCrustRB.setSelected(false);
+        deepDishCrustRB.setSelected(false);
+
+        crustPnl.add(thinCrustRB);
+        crustPnl.add(regCrustRB);
+        crustPnl.add(deepDishCrustRB);
     }
 
     private void createSizePanel() {
